8d2ea8a9d15a3afd4a30f6082ac878b1b5ddc056
FeBalla <fballadares@uc.cl>
2022-05-23 16:21:42 -0400

    hotfix: router order

--- services/backend/src/routes/api/index.js
+++ services/backend/src/routes/api/index.js
@@ -2,9 +2,9 @@ const KoaRouter = require('koa-router');
 
 const router = new KoaRouter({ prefix: '/api' });
 const users = require('./users');
-const session = require('./authentication');
+const authentication = require('./authentication');
 
-router.use('', session.routes());
 router.use('/users', users.routes());
+router.use('', authentication.routes());
 
 module.exports = router;
