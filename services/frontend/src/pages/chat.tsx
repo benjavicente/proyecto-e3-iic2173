3bbbd9a1647496db8f04d4787c28e6b20464aadb
Benjamín Vicente <benjavicente@uc.cl>
2022-06-22 01:41:15 -0400

    better ui

--- services/frontend/src/pages/chat.tsx
+++ services/frontend/src/pages/chat.tsx
@@ -1,4 +1,15 @@
-import ChatProvider, { useChat } from "~/contexts/Chat";
+import { FormEventHandler, useCallback, useState } from "react";
+import ChatProvider, { useChat, useMessagesOfChat } from "~/contexts/Chat";
+
+
+
+function ChatItem({ id, amount }) {
+  return (
+    <li className="bg-slate-600 text-slate-200 p-2">
+      {id} ({amount})
+    </li>
+  )
+}
 
 function ChatList() {
   const { chats, isLoading } = useChat();
@@ -8,23 +19,63 @@ function ChatList() {
   }
 
   return (
-    <ol>
-      <li>XD</li>
+    <ol className="flex flex-col">
+      {chats.map(chat => (<ChatItem key={chat.id} {...chat} />))}
     </ol>
   )
 }
 
+function Message({ id, message, created_at }: Message) {
+  const timestamp = new Date(created_at).toLocaleString();
+  return (
+    <li className="bg-slate-700 p-1 rounded flex-grow-0 self-left w-fit">
+      <div className="text-slate-200">{message}</div>
+      <time className="text-slate-500">({timestamp})</time>
+    </li>
+  )
+}
+
+function MessagesPanel({ currentChatID }) {
+  const [messageToSend, setMessageToSend] = useState("")
+  const { messages, isLoading, sendMessage } = useMessagesOfChat(currentChatID);
+
+  const handleSendMessage: FormEventHandler = useCallback((e) => {
+    e.preventDefault();
+    sendMessage(messageToSend);
+    setMessageToSend("");
+  }, [messageToSend, sendMessage])
+
+  return (
+    <div className="h-full flex flex-col">
+      <h2 className="p-2 text-slate-100 text-center">{currentChatID}</h2>
+      {isLoading ? (
+        <div className="flex-grow flex align-middle justify-center text-slate-300"> Loading...</div>
+      ) : (
+        <ol className="flex flex-col flex-shrink overflow-y-scroll gap-2 p-2">
+          {messages.map(message => (<Message key={message.id} {...message} />))}
+        </ol>
+      )}
+      <form onSubmit={handleSendMessage} className="flex">
+        <input type="text" placeholder="Mensaje" className="flex-grow" value={messageToSend} onChange={(e) => setMessageToSend(e.target.value)} />
+        <button type="submit" className="bg-sky-300 p-2">Enviar</button>
+      </form>
+    </div >
+  )
+}
+
 export default function ChatPage() {
+  const [currentChatID, setCurrentChatID] = useState("public")
+
   return (
     <ChatProvider>
-      <div className="grid grid-cols-[250px_1fr] bg-slate-800 h-screen">
-        <div className="flex flex-col bg-slate-700">
-          <div className="p-2">
+      <div className="grid grid-cols-[250px_1fr] bg-slate-800 grid-rows-[100vh]">
+        <div className="flex flex-col bg-slate-700 h-full">
+          <div className="px-2 py-4">
             <input type="text" placeholder="Escribir por id..." className="border-none w-full h-full grow-0 focus:ring-0 bg-slate-600 rounded text-slate-100 placeholder-slate-300" />
           </div>
-          <ChatList></ChatList>
+          <ChatList />
         </div>
-        <div></div>
+        <MessagesPanel currentChatID={currentChatID} />
       </div>
     </ChatProvider>
   )
