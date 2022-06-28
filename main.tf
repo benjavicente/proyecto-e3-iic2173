310d288761fb94c0de91b1383442c3be0f9e7cc7
Maggie Muñoz <62019819+maggiemunoz@users.noreply.github.com>
2022-06-27 20:41:49 -0400

    terraform

--- main.tf
+++ main.tf
@@ -2,32 +2,59 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 4.0"
+      version = "~> 4.16"
     }
   }
+
+  required_version = ">= 1.2.0"
 }
 
-# Main region where the resources should be created in
-# (Should be close to the location of your viewers)
 provider "aws" {
-  region = "us-west-2"
+  region  = "us-east-1"
 }
 
-# Provider used for creating the Lambda@Edge function which must be deployed
-# to us-east-1 region (Should not be changed)
-provider "aws" {
-  alias  = "global_region"
-  region = "us-east-1"
-}
+resource "aws_security_group" "app_server" {
+  name = "security-group-app-server"
 
-module "tf_next" {
-  source = "milliHQ/next-js/aws"
+  ingress {
+    from_port = 22
+    to_port = 22
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+    description = "ssh"
+  }
 
-  providers = {
-    aws.global_region = aws.global_region
+  ingress {
+    from_port = 80
+    to_port = 80
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+    description = "HTTP"
+  }
+
+  ingress {
+    from_port = 443
+    to_port = 443
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+    description = "HTTPS"
+  }
+
+  ingress {
+    from_port = 0
+    to_port = 0
+    protocol = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+    description = "All Ports Egress"
   }
 }
 
-output "cloudfront_domain_name" {
-  value = module.tf_next.cloudfront_domain_name
+resource "aws_instance" "app_server" {
+  ami           = "ami-04505e74c0741db8d"
+  instance_type = "t2.micro"
+  vpc_security_group_ids = [aws_security_group.app_server.id]
+
+  tags = {
+    Name = "Backend"
+  }
 }
